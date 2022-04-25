8eaf59227585a22e161623b50ecd040152b17a49
Jonathan Leal <jonathanleal@msn.com>
2022-04-25 19:34:13 +0200

    db switch

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_04_24_163216) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "groceries", force: :cascade do |t|
     t.string "nom"
     t.integer "quantity"
